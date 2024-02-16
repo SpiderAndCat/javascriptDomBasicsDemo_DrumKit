5522ffc76663d4395a82b28decf3e6ebc46b6173
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-02-16 14:27:03 -0700

    Play drum sound based on button clicked

--- index.js
+++ index.js
@@ -23,7 +23,47 @@ document.querySelector("button").addEventListener("click", function () {
 
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I was clicked!");
         // Handle the click
+        var buttonInnerHTML = this.innerHTML;
+        //`this` is the identoty of the object that TRIGGERED the Event Listener
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+        
+            case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+        
+            default:
+                console.log(buttonInnerHTML + "is not a valid key")
+                break;
+        }
+
     });
 }
